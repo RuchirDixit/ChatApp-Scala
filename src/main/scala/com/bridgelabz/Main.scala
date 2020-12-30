1bd63e05b12c4c9000632c309c685d37d7711a92
RuchirDixit <ruchirtd96@gmail.com>
2020-12-31 00:21:25 +0530

    [Ruchir]Added: Main class to run the application

---
+++ src/main/scala/com/bridgelabz/Main.scala
@@ -0,0 +1,27 @@
+package com.bridgelabz.Main
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Main extends App {
+  val conf = ConfigFactory.load()
+  val host = sys.env("Host")
+  val port_number = sys.env("Port_number").toInt
+  implicit val actorSystem: ActorSystem = ActorSystem("system")
+  implicit val materializer: ActorMaterializer =
+    ActorMaterializer()(actorSystem)
+  implicit val executionContext: ExecutionContextExecutor = actorSystem.dispatcher
+  val userManagementService = new UserManagementService
+  val userManagementRoutes = new UserManagementRoutes(userManagementService)
+  val routes = userManagementRoutes.routes
+
+  val bindingFuture = Http().bindAndHandle(routes, host, port_number)
+  println(s"Server online at http://localhost:8081")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => actorSystem.terminate())
+}
