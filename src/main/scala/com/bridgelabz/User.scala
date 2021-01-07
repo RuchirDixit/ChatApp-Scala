8e70cc0e92da6338e243f84fd26a35ed04133bc1
RuchirDixit <ruchirtd96@gmail.com>
2021-01-07 23:16:45 +0530

    [Ruchir]Added: User case class to save users to users collection

--- src/main/scala/com/bridgelabz/User.scala
+++ src/main/scala/com/bridgelabz/User.scala
@@ -2,7 +2,11 @@ package com.bridgelabz
 
 import play.api.libs.json.{Json, Reads}
 
-case class User(name:String,password:String,message:String)
+case class User(
+                 name: String,
+                 password: String,
+                 isVerified: Option[Boolean] = None
+               )
 object User {
   implicit val requestReads: Reads[User] = Json.reads[User]
 }
