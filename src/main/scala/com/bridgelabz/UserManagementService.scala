73251a3fb43d891699da1b03b657d9d0df1fc45d
RuchirDixit <ruchirtd96@gmail.com>
2021-01-07 23:17:59 +0530

    [Ruchir]Added: Methods with proper fields and added comments for methods

--- src/main/scala/com/bridgelabz/UserManagementService.scala
+++ src/main/scala/com/bridgelabz/UserManagementService.scala
@@ -1,11 +1,13 @@
 package com.bridgelabz.Main
 
-import com.bridgelabz.User
+import com.bridgelabz.{Chat, User}
 import com.typesafe.scalalogging.LazyLogging
+import org.bson.BsonType
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 class UserManagementService extends LazyLogging{
+
 
   /**
    *
@@ -14,14 +16,22 @@ class UserManagementService extends LazyLogging{
    */
   def userLogin(loginRequest: User): String = {
     val users = Await.result(Database_service.getUsersUsingFilter(loginRequest.name),60.seconds)
-    users.foreach(document => document.foreach(bsonStringObject =>
-      if(bsonStringObject._2.asString().getValue.equals(loginRequest.name)){
-        logger.info("Inside if value (name):" + bsonStringObject._2.asString().getValue)
+    users.foreach(document => document.foreach(bsonObject =>
+      if(bsonObject._2.asString().getValue.equals(loginRequest.name)){
+        logger.info("Inside if value (name):" + bsonObject._2.asString().getValue)
         users.foreach(document => document.foreach(bsonStringObject =>
           if(bsonStringObject._2.asString().getValue.equals(loginRequest.password)){
             logger.info("Inside if value (password):" + bsonStringObject._2.asString().getValue)
+            users.foreach(document => document.foreach(bsonObject =>
+              if(bsonObject._2.getBsonType() == BsonType.BOOLEAN){
+                if(bsonObject._2.asBoolean().getValue == false){
+                  return "User Not verified"
+                }
+              }
+            ))
             return "Login Successful"
-          }))
+          }
+        ))
       }
     ))
     "User not found"
@@ -37,8 +47,26 @@ class UserManagementService extends LazyLogging{
     if (status.equals("Success")){
       "User created"
     }
+    else if(status.equals("Validation Failed")){
+      "User Validation Failed"
+    }
     else {
       "User not created"
+    }
+  }
+
+  /**
+   *
+   * @param sendMessageRequest: Contains request of type Chat which has sender, receiver and message to be send
+   * @return : Returns String message as "Message added if message successfully sent or else "Sending Msg failed" if failed to deliver message"
+   */
+  def sendMessage(sendMessageRequest: Chat) = {
+    val status = Database_service.saveChatMessage(sendMessageRequest)
+    if(status.equals("Message sent")){
+      "Message added"
+    }
+    else {
+      "Sending Msg failed"
     }
   }
 
