c98b5c5e2046cd6bbb60e0979f3de1cf2f691dee
RuchirDixit <ruchirtd96@gmail.com>
2021-01-09 13:28:19 +0530

    [Ruchir]Refactored: Changes Chat case class to normal class to use setters

--- src/main/scala/com/bridgelabz/Chat.scala
+++ src/main/scala/com/bridgelabz/Chat.scala
@@ -1,13 +1,29 @@
 package com.bridgelabz
 
-import play.api.libs.json.{Json, Reads}
+import play.api.libs.json.{JsPath, Json, Reads, Writes}
+import play.api.libs.functional.syntax._
 
-case class Chat(
-                 sender:String,
-                 receiver:String,
-                 message:String
-               )
+class Chat(var sender:String,var receiver:String,var message:String){
+  def setSenderName(senderName:String) = {
+    sender = senderName
+  }
+  def setReceiverName(receiverName:String) = {
+    receiver = receiverName
+  }
+  def setMessage(getMessage:String) = {
+    message = getMessage
+  }
+}
 
 object Chat {
+//  implicit val citySuggestionWrites: Writes[Chat] = (
+//    (JsPath \ "name").write[String] and
+//      (JsPath \ "locationId").write[String] and
+//      (JsPath \ "locationKind").write[String]
+//    )(unlift(Chat.
+  def apply(sender: String, receiver: String, message: String): Chat
+  = new Chat(sender, receiver, message)
+
+  def unapply(arg: Chat): Option[(String, String, String)] = ???
   implicit val requestReads: Reads[Chat] = Json.reads[Chat]
 }
