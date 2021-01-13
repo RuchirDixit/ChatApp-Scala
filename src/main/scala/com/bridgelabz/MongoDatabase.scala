94d32463c2167ebd365b8591813a91824900b0f5
RuchirDixit <ruchirtd96@gmail.com>
2021-01-13 12:46:57 +0530

    [Ruchir]Refactored: Mongodb collection of Document type

--- src/main/scala/com/bridgelabz/MongoDatabase.scala
+++ src/main/scala/com/bridgelabz/MongoDatabase.scala
@@ -1,15 +1,25 @@
 package com.bridgelabz.Main
 import akka.actor.ActorSystem
-import org.mongodb.scala.{Document,MongoClient, MongoCollection, MongoDatabase}
+import com.bridgelabz.Chat
+import org.bson.codecs.configuration.CodecRegistries
+import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
+import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
+
 import scala.concurrent.ExecutionContext
 
 object MongoDatabase {
+  val chatCodecProvider = Macros.createCodecProvider[Chat]()
+
+  val codecRegistry = CodecRegistries.fromRegistries(
+    CodecRegistries.fromProviders(chatCodecProvider),
+    DEFAULT_CODEC_REGISTRY
+  )
   implicit val system = ActorSystem("Scala_jwt-App")
   implicit val executor: ExecutionContext = system.dispatcher
   val mongoClient: MongoClient = MongoClient()
   val databaseName = sys.env("database_name")
   // Getting mongodb database
-  val database: MongoDatabase = mongoClient.getDatabase(databaseName)
+  val database: MongoDatabase = mongoClient.getDatabase(databaseName).withCodecRegistry(codecRegistry)
   val registrationCollection = sys.env("register_collection_name")
   val chatCollection = sys.env("chat_collection")
   // Getting mongodb collection
