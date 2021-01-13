85171d1d7f0e2dcab76469e997cced2041cd94db
RuchirDixit <ruchirtd96@gmail.com>
2021-01-13 12:44:58 +0530

    [Ruchir]Refactored: Chat class with getters and setters

--- src/main/scala/com/bridgelabz/Chat.scala
+++ src/main/scala/com/bridgelabz/Chat.scala
@@ -1,9 +1,8 @@
 package com.bridgelabz
 
-import play.api.libs.json.{JsPath, Json, Reads, Writes}
-import play.api.libs.functional.syntax._
+import play.api.libs.json.{Json, Reads, Writes}
 
-class Chat(var sender:String,var receiver:String,var message:String){
+class Chat(var sender:String,var receiver:String,var message:String, var groupChatName:String){
   def setSenderName(senderName:String) = {
     sender = senderName
   }
@@ -13,17 +12,16 @@ class Chat(var sender:String,var receiver:String,var message:String){
   def setMessage(getMessage:String) = {
     message = getMessage
   }
+  def setGroupChatName(chatName:String) = {
+    groupChatName = chatName
+  }
 }
 
 object Chat {
-//  implicit val citySuggestionWrites: Writes[Chat] = (
-//    (JsPath \ "name").write[String] and
-//      (JsPath \ "locationId").write[String] and
-//      (JsPath \ "locationKind").write[String]
-//    )(unlift(Chat.
-  def apply(sender: String, receiver: String, message: String): Chat
-  = new Chat(sender, receiver, message)
+  def apply(sender: String, receiver: String, message: String, groupname: String): Chat
+  = new Chat(sender, receiver, message,groupname)
 
-  def unapply(arg: Chat): Option[(String, String, String)] = ???
+  def unapply(arg: Chat): Option[(String, String, String,String)] = ???
   implicit val requestReads: Reads[Chat] = Json.reads[Chat]
+  implicit val requestWrites: Writes[Chat] = Json.writes[Chat]
 }
