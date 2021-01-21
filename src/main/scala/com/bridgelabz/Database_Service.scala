e9ac63def425b6adb0f3731f88f733dde3c69628
RuchirDixit <ruchirtd96@gmail.com>
2021-01-21 18:34:47 +0530

    [Ruchir]Refactored: Removed comments

--- src/main/scala/com/bridgelabz/Database_Service.scala
+++ src/main/scala/com/bridgelabz/Database_Service.scala
@@ -1,18 +1,3 @@
-// Copyright (C) 2011-2012 the original author or authors.
-// See the LICENCE.txt file distributed with this work for additional
-// information regarding copyright ownership.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-// http://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 package com.bridgelabz.Main
 
@@ -21,14 +6,12 @@ import org.bson.BsonType
 import org.mongodb.scala.Document
 import org.mongodb.scala.model.Projections._
 import org.mongodb.scala.model.Filters._
-
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import java.util.regex.Pattern
+import com.typesafe.scalalogging.LazyLogging
 
-import com.typesafe.scalalogging.LazyLogging;
-object Database_service extends LazyLogging{
-
+object Database_service extends LazyLogging {
   /**
    *
    * @param credentials : Data about the user that is getting stored in database
@@ -99,6 +82,4 @@ object Database_service extends LazyLogging{
   def getUsersUsingFilter(name : String) : Future[Seq[Document]] = {
     MongoDatabase.collectionForUserRegistration.find(equal("name",name)).projection(excludeId()).toFuture()
   }
-
-
 }
