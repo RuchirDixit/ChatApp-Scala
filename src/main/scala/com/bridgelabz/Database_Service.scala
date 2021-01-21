c35bb9bc8063a51926353cd9524cd01cde44b1d1
RuchirDixit <ruchirtd96@gmail.com>
2021-01-21 12:00:18 +0530

    [Ruchir]Added: Save messages to chat collection

--- src/main/scala/com/bridgelabz/Database_Service.scala
+++ src/main/scala/com/bridgelabz/Database_Service.scala
@@ -1,26 +1,46 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.bridgelabz.Main
 
-import com.bridgelabz.{Chat, User}
+import com.bridgelabz.{ChatCase, User}
 import org.bson.BsonType
 import org.mongodb.scala.Document
 import org.mongodb.scala.model.Projections._
 import org.mongodb.scala.model.Filters._
-import scala.concurrent.Await
+
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-import java.util.regex.Pattern;
-object Database_service {
+import java.util.regex.Pattern
+
+import com.typesafe.scalalogging.LazyLogging;
+object Database_service extends LazyLogging{
 
   /**
    *
    * @param credentials : Data about the user that is getting stored in database
    * @return : If data is entered successfully then returns SUCCESS or else return FAILURE
    */
-  def saveUser(credentials:User) = {
+  def saveUser(credentials:User) : String = {
     val emailRegexPattern = "^[a-zA-Z]+([+.#&_-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,3}([.][a-zA-Z]{2,3})*$"
-    if(Pattern.compile(credentials.name).matcher(emailRegexPattern).matches() == true){
+    if(Pattern.compile(emailRegexPattern).matcher(credentials.name).matches()){
+      logger.info("Email valid!")
       val userToBeAdded : Document = Document("id" -> credentials.id, "name" -> credentials.name, "password" -> credentials.password, "isVerified" -> false)
       val ifUserExists = checkIfExists(credentials.name)
-      if(ifUserExists == true)
+      if(ifUserExists)
       {
         "Failure"
       }
@@ -32,6 +52,7 @@ object Database_service {
       }
     }
     else {
+      logger.info("Invalid email")
       "Validation Failed"
     }
   }
@@ -45,8 +66,9 @@ object Database_service {
     val data = Await.result(getUsers(),10.seconds)
     data.foreach(document => document.foreach(bsonObject =>
       if(bsonObject._2.getBsonType() == BsonType.STRING){
-        if(bsonObject._2.asString().getValue().equals(name))
+        if(bsonObject._2.asString().getValue().equals(name)) {
           return true
+        }
       }
     ))
     false
@@ -57,15 +79,15 @@ object Database_service {
    * @param sendMessageRequest: The data about sender, receiver and the message that is to be send; added to chat-log collection
    * @return : String "Message sent" to inform that message is saved to database
    */
-  def saveChatMessage(sendMessageRequest: Chat) : String = {
-    val senderToReceiverMessage : Document = Document("sender" -> sendMessageRequest.sender, "receiver" -> sendMessageRequest.receiver, "message" -> sendMessageRequest.message, "groupChatName" -> sendMessageRequest.groupChatName)
-    val chatAddedFuture = MongoDatabase.collectionForChat.insertOne(senderToReceiverMessage).toFuture()
+  def saveChatMessage(sendMessageRequest: ChatCase) : String = {
+    val data = ChatCase(sendMessageRequest.sender,sendMessageRequest.receiver,sendMessageRequest.message,sendMessageRequest.groupChatName)
+    val chatAddedFuture = MongoDatabase.collectionForChat.insertOne(data).toFuture()
     Await.result(chatAddedFuture,60.seconds)
     "Message sent"
   }
 
   // Returns All the users present inside database in the form of future
-  def getUsers() = {
+  def getUsers() : Future[Seq[Document]] = {
     MongoDatabase.collectionForUserRegistration.find().toFuture()
   }
 
@@ -74,7 +96,7 @@ object Database_service {
    * @param name : Find user based on name specified
    * @return : Future of users that are returned using find method from database
    */
-  def getUsersUsingFilter(name : String) = {
+  def getUsersUsingFilter(name : String) : Future[Seq[Document]] = {
     MongoDatabase.collectionForUserRegistration.find(equal("name",name)).projection(excludeId()).toFuture()
   }
 
