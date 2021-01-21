ec69906e26e0d231b90a8a9aad4f4ff7ebf266ac
RuchirDixit <ruchirtd96@gmail.com>
2021-01-21 12:04:41 +0530

    [Ruchir]Refactored: Document type to ChatCase type in mongo collection

--- src/main/scala/com/bridgelabz/MongoDatabase.scala
+++ src/main/scala/com/bridgelabz/MongoDatabase.scala
@@ -1,6 +1,22 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.Main
+
 import akka.actor.ActorSystem
-import com.bridgelabz.Chat
+import com.bridgelabz.ChatCase
 import org.bson.codecs.configuration.CodecRegistries
 import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
 import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
@@ -8,7 +24,7 @@ import org.mongodb.scala.{Document, MongoClient, MongoCollection, MongoDatabase}
 import scala.concurrent.ExecutionContext
 
 object MongoDatabase {
-  val chatCodecProvider = Macros.createCodecProvider[Chat]()
+  val chatCodecProvider = Macros.createCodecProvider[ChatCase]()
 
   val codecRegistry = CodecRegistries.fromRegistries(
     CodecRegistries.fromProviders(chatCodecProvider),
@@ -25,6 +41,6 @@ object MongoDatabase {
   // Getting mongodb collection
   val collectionForUserRegistration: MongoCollection[Document] = database.getCollection(registrationCollection)
   collectionForUserRegistration.drop()
-  val collectionForChat: MongoCollection[Document] = database.getCollection(chatCollection)
+  val collectionForChat: MongoCollection[ChatCase] = database.getCollection[ChatCase](chatCollection)
   collectionForChat.drop()
 }
