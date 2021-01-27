c93213b570c01d5d6243a8c361e71cec59934609
RuchirDixit <ruchirtd96@gmail.com>
2021-01-27 12:15:20 +0530

    [Ruchir]Refactored: Moved MyJsonProtocol trait to marshallers package

---
+++ src/main/scala/com/bridgelabz/marshallers/MyJsonProtocol.scala
@@ -0,0 +1,12 @@
+
+package com.bridgelabz.marshallers
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import com.bridgelabz.caseClasses.{ChatCase, GroupChat}
+import spray.json.DefaultJsonProtocol
+
+// To convert object into json format
+trait MyJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val templateFormat = jsonFormat4(ChatCase)
+  implicit val templateFormatGroup = jsonFormat3(GroupChat)
+}
