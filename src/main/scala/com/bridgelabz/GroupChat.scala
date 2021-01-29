95f7542d331f14202120efffc8f7673c0658bad2
Ruchir Dixit <ruchirtd96@gmail.com>
2021-01-29 18:24:07 +0530

    [Ruchir]: Deleted: GroupChatscala file

--- src/main/scala/com/bridgelabz/GroupChat.scala
+++
@@ -1,9 +0,0 @@
-
-package com.bridgelabz
-
-import play.api.libs.json.{Json, Reads}
-
-case class GroupChat(
-                      sender:Option[String],
-                      receiver:String,
-                      message:String)
