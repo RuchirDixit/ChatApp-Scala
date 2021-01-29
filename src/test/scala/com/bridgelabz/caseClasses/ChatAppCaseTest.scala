753d46100cea02ea3b232f5f20fe76afc09299c8
RuchirDixit <ruchirtd96@gmail.com>
2021-01-29 12:12:33 +0530

    [Ruchir]WIP: To test functioning of case classes

---
+++ src/test/scala/com/bridgelabz/caseClasses/ChatAppCaseTest.scala
@@ -0,0 +1,15 @@
+
+package com.bridgelabz.caseClasses
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.matchers.should
+import org.scalatest.wordspec.AnyWordSpec
+
+class ChatAppCaseTest extends AnyWordSpec with should.Matchers with ScalatestRouteTest {
+  "Check if sender exists" should {
+    "return true" in {
+      val status = ChatCase(Some("ruchircool007@gmail.com"),"xyz@gmail.com","hey",Some(""))
+      assert(status == ChatCase(Some("ruchircool007@gmail.com"),"xyz@gmail.com","hey",Some("")))
+    }
+  }
+}
