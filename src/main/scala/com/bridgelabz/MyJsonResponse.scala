0d90153a510452c9a76c5b5f7be1ddb227a0fa46
Ruchir Dixit <ruchirtd96@gmail.com>
2021-01-29 18:26:29 +0530

    [Ruchir]: Deleted: MyJsonResponse.scala

--- src/main/scala/com/bridgelabz/MyJsonResponse.scala
+++
@@ -1,9 +0,0 @@
-
-package com.bridgelabz
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.DefaultJsonProtocol
-
-trait MyJsonResponse extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val itemFormat = jsonFormat1(JsonResponse)
-}
