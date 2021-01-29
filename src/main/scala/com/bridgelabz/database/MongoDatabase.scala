3e2745ed8a3adaa2cd9c6650669d9a6950670683
RuchirDixit <ruchirtd96@gmail.com>
2021-01-29 12:07:21 +0530

    [Ruchir]Refactored: Removed creation of actor system

--- src/main/scala/com/bridgelabz/database/MongoDatabase.scala
+++ src/main/scala/com/bridgelabz/database/MongoDatabase.scala
@@ -2,6 +2,7 @@
 package com.bridgelabz.database
 
 import akka.actor.ActorSystem
+import com.bridgelabz.actors.ActorSystemFactory
 import com.bridgelabz.caseClasses.{ChatCase, GroupChat}
 import org.bson.codecs.configuration.CodecRegistries
 import org.mongodb.scala.bson.codecs.{DEFAULT_CODEC_REGISTRY, Macros}
@@ -16,15 +17,20 @@ object MongoDatabase {
     CodecRegistries.fromProviders(chatCodecProvider,groupCodecProvider),
     DEFAULT_CODEC_REGISTRY
   )
-  implicit val system = ActorSystem("Scala_jwt-App")
+  //implicit val system = ActorSystem("Scala_jwt-App")
+  implicit val system = ActorSystemFactory.system
   implicit val executor: ExecutionContext = system.dispatcher
   val mongoClient: MongoClient = MongoClient()
-  val databaseName = sys.env("database_name")
+  //val databaseName = sys.env("database_name")
+  val databaseName = "ChatApp"
   // Getting mongodb database
   val database: MongoDatabase = mongoClient.getDatabase(databaseName).withCodecRegistry(codecRegistry)
-  val registrationCollection = sys.env("register_collection_name")
-  val chatCollection = sys.env("chat_collection")
-  val groupCollection = sys.env("group-collection")
+  //val registrationCollection = sys.env("register_collection_name")
+  val registrationCollection = "users"
+ // val chatCollection = sys.env("chat_collection")
+ val chatCollection = "chat_logs"
+  //val groupCollection = sys.env("group-collection")
+  val groupCollection = "group-chats"
   // Getting mongodb collection
   val collectionForUserRegistration: MongoCollection[Document] = database.getCollection(registrationCollection)
   collectionForUserRegistration.drop()
