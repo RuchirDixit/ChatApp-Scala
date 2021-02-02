08e5cc853408ba59d349ee175141135b6e68d4b8
RuchirDixit <ruchirtd96@gmail.com>
2021-02-02 22:14:34 +0530

    [Ruchir]Added: Header to fix scalastyle warning in routes package

--- src/main/scala/com/bridgelabz/routes/UserManagementRoutes.scala
+++ src/main/scala/com/bridgelabz/routes/UserManagementRoutes.scala
@@ -1,4 +1,18 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.routes
 
 import akka.actor.{ActorSystem, Props}
@@ -30,7 +44,6 @@ class UserManagementRoutes(service: UserManagementService) extends PlayJsonSuppo
   with MyJsonProtocol with MyJsonResponse {
   implicit val system = ActorSystemFactory.system
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
-  val saveMessageActor = system.actorOf(Props[SaveToDatabaseActor], "saveActor")
   val routes: Route =
     pathPrefix("user") {
         /**
@@ -175,6 +188,7 @@ class UserManagementRoutes(service: UserManagementService) extends PlayJsonSuppo
                   val groupChatName = userService.generateGroupChatName(token.values.toList.last.toString(), getDataToSaveChats.receiver, senderId, receiverId)
                   val saveToChat = getDataToSaveChats.copy(sender = Some(token.values.toList.last.toString()),groupChatName = Some(groupChatName))
                   implicit val timeout = Timeout(10.seconds)
+                  val saveMessageActor = system.actorOf(Props[SaveToDatabaseActor], "saveActor")
                   val futureResponse = saveMessageActor ? saveToChat
                   onComplete(futureResponse) {
                     case Success(result) =>
