538784665e4fa45f1193b8ed8f3513e050813d8e
RuchirDixit <ruchirtd96@gmail.com>
2021-02-02 22:15:33 +0530

    [Ruchir]Added: Header to fix scalastyle warning in traits package

--- src/test/scala/com/bridgelabz/caseclasses/ChatAppCaseTest.scala
+++ src/test/scala/com/bridgelabz/caseclasses/ChatAppCaseTest.scala
@@ -1,4 +1,18 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.caseclasses
 
 import akka.http.scaladsl.testkit.ScalatestRouteTest
@@ -6,10 +20,18 @@ import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
 
 class ChatAppCaseTest extends AnyWordSpec with should.Matchers with ScalatestRouteTest {
-  "Check if sender exists" should {
-    "return true" in {
+  "Check if test case passes" should {
+    "return ChatCase created true" in {
       val status = ChatCase(Some("ruchircool007@gmail.com"),"xyz@gmail.com","hey",Some(""))
       assert(status == ChatCase(Some("ruchircool007@gmail.com"),"xyz@gmail.com","hey",Some("")))
+    }
+    "return GroupChat created true" in {
+      val status = GroupChat(Some("ruchircool007@gmail.com"),"Future of India","hola")
+      assert(status == GroupChat(Some("ruchircool007@gmail.com"),"Future of India","hola"))
+    }
+    "return GroupMessages created true" in {
+      val status = GroupMessages("Future of India")
+      assert(status == GroupMessages("Future of India"))
     }
   }
 }
