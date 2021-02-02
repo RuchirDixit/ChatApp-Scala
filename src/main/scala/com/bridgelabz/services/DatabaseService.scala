38bee63ff3ee53b061ad1ca4c77a2e0941851ac7
RuchirDixit <ruchirtd96@gmail.com>
2021-02-02 22:14:45 +0530

    [Ruchir]Added: Header to fix scalastyle warning in services package

--- src/main/scala/com/bridgelabz/services/DatabaseService.scala
+++ src/main/scala/com/bridgelabz/services/DatabaseService.scala
@@ -1,8 +1,22 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.services
 
 import java.util.regex.Pattern
-
+import scala.util.control.Breaks._
 import akka.http.scaladsl.server.Directives.complete
 import com.bridgelabz.caseclasses.{ChatCase, GroupChat, JsonResponse, User}
 import com.bridgelabz.database.MongoDatabase
@@ -80,8 +94,8 @@ object DatabaseService extends LazyLogging with DatabaseService {
       false
     }
     catch {
-      case _: TimeoutException =>
-        logger.error("Timeout exception for getting users")
+      case exception: TimeoutException =>
+        logger.error(exception.toString)
         false
     }
   }
