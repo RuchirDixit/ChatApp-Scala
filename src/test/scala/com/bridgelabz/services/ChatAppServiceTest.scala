63fb1a194c7ed4ab6d790fb4c768d93bc7f87530
RuchirDixit <ruchirtd96@gmail.com>
2021-02-02 22:15:59 +0530

    [Ruchir]Added: Header to fix scalastyle warning in services package

--- src/test/scala/com/bridgelabz/services/ChatAppServiceTest.scala
+++ src/test/scala/com/bridgelabz/services/ChatAppServiceTest.scala
@@ -1,8 +1,22 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.services
 
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.bridgelabz.caseclasses.{ChatCase, User}
+import com.bridgelabz.caseclasses.{ChatCase, GroupChat, User}
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -47,6 +61,15 @@ class ChatAppServiceTest extends AnyWordSpec with should.Matchers with Scalatest
     }
   }
 
+  // Test case to successfully save chat messages for group interaction
+  "On Successful chat sent to group" should {
+    "return Message sent to group" in {
+      val data = GroupChat(Some("ruchircool007@gmail.com"),"Test case group","Hey group")
+      val status = DatabaseService.saveToGroupChat(data)
+      assert(status == "Message sent to group")
+    }
+  }
+
   // Test case to check if user is logged in or not
   "On Passing credentials" should {
     val service = new UserManagementService
@@ -59,6 +82,59 @@ class ChatAppServiceTest extends AnyWordSpec with should.Matchers with Scalatest
       val data = User(Some(1),"ruchir@rediffmail.com","demo",Some(false))
       val status = service.userLogin(data)
       assert(status == "User not found")
+    }
+  }
+
+  // Test case to check if user is created or not
+  "On Passing credentials" should {
+    val service = new UserManagementService
+    "return User created " in {
+      val data = User(Some(1),"ruchir32@gmail.com","demoemail",Some(true))
+      val status = service.createUser(data)
+      assert(status == "User created")
+    }
+    "return User not created" in {
+      val data = User(Some(1),"ruchir96@gmail.com","demoemail",Some(false))
+      val status = service.createUser(data)
+      assert(status == "User not created")
+    }
+  }
+
+  // Test case to check if message is sent or not
+  "On Passing ChatCase credentials" should {
+    val service = new UserManagementService
+    "return Message Added " in {
+      val data = ChatCase(Some("ruchirtd96@gmail.com"),"ruchircool007@gmail.com","test message",Some("test group"))
+      val status = service.sendMessage(data)
+      assert(status == "Message added")
+    }
+  }
+
+  // Test case to get ID for a particular name
+  "On Passing name of user" should {
+    val service = new UserManagementService
+    "return Id" in {
+      val status = service.returnId("ruchirtd96@gmail.com")
+      assert(status == 1)
+    }
+  }
+
+  // Test case to create new group name
+  "On Passing name of user" should {
+    val service = new UserManagementService
+    "return new group name" in {
+      val status = service.generateGroupChatName("ruchirtd96@gmail.com","xyz@gmail.com","1",2)
+      assert(status == "rx-12")
+    }
+    }
+
+  // Test case to create new group chat
+  "On Passing GroupChat credentials" should {
+    val service = new UserManagementService
+    "return Message added" in {
+      val groupChat = GroupChat(Some("ruchirtd96@gmail.com"),"xyz@gmail.com","Group created")
+      val status = service.saveGroupChat(groupChat)
+      assert(status == "Message added")
     }
   }
 }
