85581f9c3c87be01c70bef6f7d9d3deea2dc1d65
RuchirDixit <ruchirtd96@gmail.com>
2021-02-02 22:36:28 +0530

    [Ruchir]Added: Save to database actor to database package

---
+++ src/main/scala/com/bridgelabz/database/SaveToDatabaseActor.scala
@@ -0,0 +1,31 @@
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.bridgelabz.database
+
+import akka.actor.Actor
+import com.bridgelabz.caseclasses.ChatCase
+import com.bridgelabz.services.UserManagementService
+import com.typesafe.scalalogging.LazyLogging
+
+class SaveToDatabaseActor extends Actor with LazyLogging{
+  override def receive: Receive = {
+    case chat:ChatCase =>
+      val userManagementService = new UserManagementService
+      val response = userManagementService.sendMessage(chat)
+      logger.info(response)
+      sender() ! response
+  }
+}
