516bb08391b74036b1c2608749c057e7a893d319
RuchirDixit <ruchirtd96@gmail.com>
2021-02-06 00:54:26 +0530

    [Ruchir]Added: Chat App routes test

--- src/test/scala/com/bridgelabz/routes/ChatAppRouteTest.scala
+++ src/test/scala/com/bridgelabz/routes/ChatAppRouteTest.scala
@@ -19,22 +19,26 @@ import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.util.ByteString
 import com.bridgelabz.services.UserManagementService
+import com.typesafe.sslconfig.ssl.FakeChainedKeyStore.User
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.mockito.MockitoSugar
+import org.mockito.Mockito.when
 
-class ChatAppRouteTest extends AnyWordSpec with should.Matchers with ScalatestRouteTest  {
+class ChatAppRouteTest extends AnyWordSpec with should.Matchers with ScalatestRouteTest with MockitoSugar  {
   "A Router" should {
     "register successfully" in {
+      val regMock = mock[UserManagementService]
       val jsonRequest = ByteString(
         s"""
            |{
-           |    "name":"ruchirtd96@gmail.com",
+           |    "name":"ruchirtd302@gmail.com",
            |    "password":"demo"
            |}
               """.stripMargin)
       Post("/user/register", HttpEntity(MediaTypes.`application/json`, jsonRequest)) ~>
         new UserManagementRoutes(new UserManagementService).routes ~> check {
-        assert(status == StatusCodes.Unauthorized)
+        assert(status == StatusCodes.OK)
       }
     }
   }
@@ -55,12 +59,28 @@ class ChatAppRouteTest extends AnyWordSpec with should.Matchers with ScalatestRo
     }
   }
 
+  "A Router" should {
+    "return bad request" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "name":"@gmail.com",
+           |    "password":"demo"
+           |}
+              """.stripMargin)
+      Post("/user/register", HttpEntity(MediaTypes.`application/json`, jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check {
+        assert(status == StatusCodes.BadRequest)
+      }
+    }
+  }
+
     "A router" should {
       "login successfully" in {
         val jsonRequest = ByteString(
           s"""
              |{
-             |    "name":"ruchirtd96@gmail.com",
+             |    "name":"ruchir99@gmail.com",
              |    "password":"demoemail",
              |    "isVerified":true
              |}
@@ -71,5 +91,117 @@ class ChatAppRouteTest extends AnyWordSpec with should.Matchers with ScalatestRo
           assert(status == StatusCodes.OK)
         }
       }
+  }
+  "A router" should {
+    "return unavailable for legal reasons" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "name":"xyz@gmail.com",
+           |    "password":"1234",
+           |    "isVerified":false
+           |}
+              """.stripMargin)
+      Post("/user/login", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.UnavailableForLegalReasons)
+      }
+    }
+  }
+  "A router" should {
+    "return unauthorized" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "name":"ruchir99@gmail.com",
+           |    "password":"de",
+           |    "isVerified":true
+           |}
+              """.stripMargin)
+      Post("/user/login", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.Unauthorized)
+      }
+    }
+  }
+  "A router" should {
+    "return verified" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "sender":"ruchir99@gmail.com",
+           |    "receiver":"xyz@gmail.com"
+           |}
+              """.stripMargin)
+      Post("/user/messages", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.OK)
+      }
+    }
+  }
+  "A router" should {
+    "return internal error" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |    "receiver":"demogroup",
+           |    "message":"hey"
+           |}
+              """.stripMargin)
+      Post("/user/groupChat", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.InternalServerError)
+      }
+    }
+  }
+  "A router" should {
+    "return 500 error" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |
+           |}
+              """.stripMargin)
+      Post("/user/viewGroups", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.InternalServerError)
+      }
+    }
+  }
+  "A router" should {
+    "return internal server error for authorize" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |  "receiver":"xyz@gmail.com",
+           |   "message":"Hey ruchir"
+           |}
+              """.stripMargin)
+      Post("/user/authorize", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.InternalServerError)
+      }
+    }
+  }
+  "A router" should {
+    "return OK" in {
+      val jsonRequest = ByteString(
+        s"""
+           |{
+           |
+           |}
+              """.stripMargin)
+      Get("/user/", HttpEntity(MediaTypes.`application/json`,jsonRequest)) ~>
+        new UserManagementRoutes(new UserManagementService).routes ~> check
+      {
+        assert(status == StatusCodes.OK)
+      }
+    }
   }
 }
