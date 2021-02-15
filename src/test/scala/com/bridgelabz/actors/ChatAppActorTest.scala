2b709f44d04d2aea2707db992c3c0a31bcb68471
RuchirDixit <ruchirtd96@gmail.com>
2021-02-15 12:51:38 +0530

    [Ruchir]Added: Test cases to test actors

--- src/test/scala/com/bridgelabz/actors/ChatAppActorTest.scala
+++ src/test/scala/com/bridgelabz/actors/ChatAppActorTest.scala
@@ -1,10 +1,25 @@
-
+// Copyright (C) 2011-2012 the original author or authors.
+// See the LICENCE.txt file distributed with this work for additional
+// information regarding copyright ownership.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.bridgelabz.actors
 
 import akka.actor.{ActorSystem, Props}
-import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import akka.testkit.{ImplicitSender, TestActors, TestKit, TestProbe}
 import com.bridgelabz.caseclasses.ChatCase
 import com.bridgelabz.database.SaveToDatabaseActor
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
@@ -14,7 +29,7 @@ import scala.concurrent.ExecutionContextExecutor
 class ChatAppActorTest extends TestKit(ActorSystem("ChatApp-test")) with ImplicitSender
   with AnyWordSpecLike
   with Matchers
-  with BeforeAndAfterAll{
+  with BeforeAndAfterAll with LazyLogging{
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   "An Email notification actor" must {
     "expect no message" in {
@@ -30,6 +45,14 @@ class ChatAppActorTest extends TestKit(ActorSystem("ChatApp-test")) with Implici
       val chatChatData = ChatCase(Some("ruchir99@gmail.com"),"xyz@gmail.com","test actor",Some("test actor group"))
       saveToDatabaseActor ! chatChatData
       expectMsg("Message added")
+    }
+  }
+
+  "An Email notification actor" must {
+    "expect no message for email" in {
+      val emailActor = system.actorOf(Props(new EmailNotificationActor))
+      emailActor ! ""
+      expectNoMessage()
     }
   }
 }
