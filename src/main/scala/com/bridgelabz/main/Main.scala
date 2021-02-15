6ee95db282acbf5b7824d8a933b90f36160f3fab
RuchirDixit <ruchirtd96@gmail.com>
2021-02-15 12:40:48 +0530

    [Ruchir]Refactored: Single actor system

--- src/main/scala/com/bridgelabz/main/Main.scala
+++ src/main/scala/com/bridgelabz/main/Main.scala
@@ -15,7 +15,6 @@
 // limitations under the License.
 package com.bridgelabz.main
 
-import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.bridgelabz.actors.ActorSystemFactory
@@ -23,16 +22,13 @@ import com.bridgelabz.routes.UserManagementRoutes
 import com.bridgelabz.services.UserManagementService
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
-
 import scala.concurrent.ExecutionContext
 import scala.io.StdIn
 
-// Singleton Pattern
 object Main extends App with LazyLogging {
-  private val conf = ConfigFactory.load()
+  private val _ = ConfigFactory.load()
   private val host = sys.env("Host")
   private val port_number = sys.env("Port_number").toInt
-  //implicit val system = ActorSystem("QuickStart")
   implicit val system = ActorSystemFactory.system
   implicit val mat = ActorMaterializer()
   implicit val executor: ExecutionContext = system.dispatcher
