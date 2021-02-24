69fe075fbe0d02aed860163108ca4b50029a357d
RuchirDixit <ruchirtd96@gmail.com>
2021-02-24 22:28:43 +0530

    [Ruchir]Refactored: Removed mail method for to follow single responsibility

--- src/main/scala/com/bridgelabz/services/UserManagementService.scala
+++ src/main/scala/com/bridgelabz/services/UserManagementService.scala
@@ -15,18 +15,13 @@
 // limitations under the License.
 package com.bridgelabz.services
 
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Directives.complete
 import com.bridgelabz.actors.ActorSystemFactory
 import com.bridgelabz.caseclasses.{ChatCase, GroupChat, User}
 import com.bridgelabz.database.DatabaseService
 import com.typesafe.scalalogging.LazyLogging
-import courier.{Envelope, Mailer, Text}
-import javax.mail.internet.InternetAddress
 import org.bson.BsonType
 import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContextExecutor}
-import scala.util.{Failure, Success}
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 
 class UserManagementService extends LazyLogging {
 
@@ -38,31 +33,33 @@ class UserManagementService extends LazyLogging {
    * Fetches data from database and checks if credentials passed matches for login
    * @return : If successful login return "Login Successful"
    */
-  def userLogin(loginRequest: User): String = {
-    val users = Await.result(DatabaseService.getUsersUsingFilter(loginRequest.name),60.seconds)
-    users.foreach(document => document.foreach(bsonObject =>
-      if(bsonObject._2.getBsonType() == BsonType.STRING) {
-        if(bsonObject._2.asString().getValue.equals(loginRequest.name)){
-          logger.info("Inside if value (name):" + bsonObject._2.asString().getValue)
-          users.foreach(document => document.foreach(bsonStringObject =>
-            if(bsonStringObject._2.getBsonType() == BsonType.STRING){
-              if(bsonStringObject._2.asString().getValue.equals(loginRequest.password)){
-                logger.info("Inside if value (password):" + bsonStringObject._2.asString().getValue)
-                users.foreach(document => document.foreach(bsonObject =>
-                  if(bsonObject._2.getBsonType() == BsonType.BOOLEAN){
-                    if(!bsonObject._2.asBoolean().getValue){
-                      return "User Not verified"
+  def userLogin(loginRequest: User): Future[String] = {
+    //val users = Await.result(DatabaseService.getUsersUsingFilter(loginRequest.name),60.seconds)
+    DatabaseService.getUsersUsingFilter(loginRequest.name).map(users => {
+      users.foreach(document => document.foreach(bsonObject =>
+        if(bsonObject._2.getBsonType() == BsonType.STRING) {
+          if(bsonObject._2.asString().getValue.equals(loginRequest.name)){
+            logger.info("Inside if value (name):" + bsonObject._2.asString().getValue)
+            users.foreach(document => document.foreach(bsonStringObject =>
+              if(bsonStringObject._2.getBsonType() == BsonType.STRING){
+                if(bsonStringObject._2.asString().getValue.equals(loginRequest.password)){
+                  logger.info("Inside if value (password):" + bsonStringObject._2.asString().getValue)
+                  users.foreach(document => document.foreach(bsonObject =>
+                    if(bsonObject._2.getBsonType() == BsonType.BOOLEAN){
+                      if(!bsonObject._2.asBoolean().getValue){
+                        return Future("User Not verified")
+                      }
                     }
-                  }
-                ))
-                return "Login Successful"
+                  ))
+                  return Future("Login Successful")
+                }
               }
-            }
-          ))
+            ))
+          }
         }
-      }
-    ))
-    "User not found"
+      ))
+      "User not found"
+    })
   }
 
   /**
@@ -71,19 +68,10 @@ class UserManagementService extends LazyLogging {
    *  It is used to save users
    * @return : If successful login return "User created"
    */
-  def createUser(createUserRequest: User):  String = {
+  def createUser(createUserRequest: User):  Future[String] = {
     val status = DatabaseService.saveUser(createUserRequest)
-    logger.info("service" + DatabaseService.saveUser(createUserRequest))
-    if (status.equals("Success")){
-      "User created"
+     status
     }
-    else if(status.equals("Validation Failed")){
-      "User Validation Failed"
-    }
-    else {
-      "User not created"
-    }
-  }
 
   /**
    *
@@ -105,7 +93,7 @@ class UserManagementService extends LazyLogging {
    *
    * @param name : Accepts name of user whose ID we have to find
    *  It searches for all users and returns ID of mentioned user
-   * @return : Id of user with speicified names
+   * @return : Id of user with specified names
    */
   def returnId(name: String) : Int = {
     val users = Await.result(DatabaseService.getUsersUsingFilter(name),60.seconds)
@@ -131,28 +119,6 @@ class UserManagementService extends LazyLogging {
     val idString = senderId + "" + receiverId
     logger.info("Final name:" + nameString.sorted + "-" + idString.sorted)
     nameString.sorted + "-" + idString.sorted
-  }
-
-  /**
-   *
-   * @param receiverAddress : To whom we have to send email
-   *  Uses courier library to send email to registered user
-   *  @return : Success if email send successfully or NOT Found if not able to send mail
-   */
-  def sendEmailReminder(receiverAddress:String) : String = {
-    val mailer = Mailer(sys.env("GMAILMAILER"), sys.env("SMTPPORT").toInt)
-      .auth(true)
-      .as(sys.env("SENDEREMAIL"),sys.env("PASSWORD"))
-      .startTls(true)()
-    mailer(Envelope.from(new InternetAddress(sys.env("SENDEREMAIL")))
-      .to(new InternetAddress(receiverAddress))
-      .subject("Full version Reminder")
-      .content(Text("Purchase full version for more exciting features for chatting. Pay $20 for full version. Buy Now!")))
-      .onComplete {
-        case Success(_) => logger.info("Email notification Sent!")
-        case Failure(_) => complete(StatusCodes.NotFound,"Failed To Deliver Notification!")
-      }
-    "Mail send"
   }
 
   /**
